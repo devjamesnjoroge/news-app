f1b12f564482f16bd86e70b3ed6e82aff6a25f43
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-01 21:19:09 +0300

    ft: add dynamic route to source articles

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,14 @@
 from flask import render_template, url_for
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources, get_articles
 
 
 @main.route('/')
 def index():
     news_sources = get_sources()
     return render_template('index.html', news_sources = news_sources)
+
+@main.route('/source/<id>')
+def source(id):
+    source = get_articles(id)
+    return render_template('source.html', source = source)
