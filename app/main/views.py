737eb090fbdd724547fe830e443a27e88d81cd0e
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-01 17:50:48 +0300

    ft: add a get request for sources url

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template
 from . import main
+from ..requests import get_sources
 
 
 @main.route('/')
 def index():
+    get_sources()
     return render_template('index.html')
--- app/requests.py
+++ app/requests.py
@@ -14,3 +14,19 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     sources_url = app.config['NEWS_API_SOURCE_URL']
     articles_url = app.config['NEWS_API_ARTICLES_URL']
+
+
+def get_sources():
+    get_sources_url = sources_url.format(api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+        
+        sources_results = None
+
+        if get_sources_response['sources']:
+            sources_results_list = get_sources_response['sources']
+            sources_results = process_sources(sources_results_list)
+    
+    return sources_results
