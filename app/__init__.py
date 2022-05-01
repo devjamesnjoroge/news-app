60aa47423b91884835e91ee7243bf558e99598ea
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-01 15:31:27 +0300

    ft: activated manage.py to run app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from config import config_options
 
 
 def create_app(config_name):
 
     app = Flask(__name__)
+
+     # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
     #register blueprint
     from .main import main as main_blueprint
