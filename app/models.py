a206759235047942521469f978f6f25786f8192b
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-05-01 17:06:46 +0300

    ft: add news articles model

--- app/models.py
+++ app/models.py
@@ -7,3 +7,14 @@ class Sources:
         self.category = category
         self.language = language
         self.country = country
+
+
+class Articles:
+    def __init__(self, author, title, description, url, urlToImage, publishedAt, content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
